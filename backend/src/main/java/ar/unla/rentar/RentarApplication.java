4aa920c425bd87d069dbf9d7891f0a80c46b57c7
Johann gomez oriel <johannsabe1@gmail.com>
2026-09-07 11:34:10 -0300

    Base de proyecto Rentar

---
+++ backend/src/main/java/ar/unla/rentar/RentarApplication.java
@@ -0,0 +1,13 @@
+package ar.unla.rentar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentarApplication.class, args);
+	}
+
+}
