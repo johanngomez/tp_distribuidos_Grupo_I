1682e6ee1c4b361b2483d0f86cfa85b63125c975
Johann gomez oriel <johannsabe1@gmail.com>
2026-09-07 12:35:33 -0300

    implementnamos reglas de negocios en services dadas en el TP, que al crear se verifique si un vehiculo ya existe y que cuando se crea este disponible y activo luego en modificar y el eliminar se busca el id para ver si existe , en eliminar solo es una baja logica pasa de activo true afalse , mientras que en modificar se guarda todo menos la patente que no se puede modificar

--- backend/src/main/java/ar/unla/rentar/service/VehiculoService.java
+++ backend/src/main/java/ar/unla/rentar/service/VehiculoService.java
@@ -1,5 +1,6 @@
 package ar.unla.rentar.service;
 
+import ar.unla.rentar.model.EstadoVehiculo;
 import ar.unla.rentar.model.Vehiculo;
 import ar.unla.rentar.repository.VehiculoRepository;
 import org.springframework.stereotype.Service;
@@ -25,14 +26,44 @@ public class VehiculoService {
     }
 
     public Vehiculo crear(Vehiculo vehiculo) {
+
+        // Verificar si ya existe un vehículo con la misma patente
+        if (vehiculoRepository.existsByPatente(vehiculo.getPatente())) {
+            throw new IllegalArgumentException("Ya existe un vehículo con esa patente");
+        }
+         // Establecer el estado y la disponibilidad del vehículo antes de guardarlo
+        vehiculo.setEstado(EstadoVehiculo.DISPONIBLE);
+        vehiculo.setActivo(true);
+
         return vehiculoRepository.save(vehiculo);
     }
 
     public Vehiculo modificar(Long id, Vehiculo vehiculo) {
-        return vehiculoRepository.save(vehiculo);
+
+        // Verificar si el vehículo con el ID proporcionado existe
+        Vehiculo vehiculoExistente = vehiculoRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("No existe un vehículo con ese ID"));
+
+        vehiculoExistente.setMarca(vehiculo.getMarca());
+        vehiculoExistente.setModelo(vehiculo.getModelo());
+        vehiculoExistente.setAnio(vehiculo.getAnio());
+        vehiculoExistente.setColor(vehiculo.getColor());
+        vehiculoExistente.setTipo(vehiculo.getTipo());
+        vehiculoExistente.setPrecioDiario(vehiculo.getPrecioDiario());
+        vehiculoExistente.setEstado(vehiculo.getEstado());
+
+
+        return vehiculoRepository.save(vehiculoExistente); // Guardar los cambios en la base de datos
     }
 
-    public void eliminar(Long id) {
-        vehiculoRepository.deleteById(id);
+    public Vehiculo eliminar(Long id) {
+        // Verificar si el vehículo con el ID proporcionado existe
+        Vehiculo vehiculo = vehiculoRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("No existe un vehículo con ese ID"));
+
+        // En lugar de eliminar físicamente el vehículo, se marca como inactivo baja lógica
+        vehiculo.setActivo(false);
+
+        return vehiculoRepository.save(vehiculo);
     }
 }
