6223aa728118199dbe3389e11b3f3915caccc50c
unknown <pauloargibay@gmail.com>
2026-09-10 19:45:45 -0300

    primer implementación de ReservaTest

--- backend/src/test/java/ar/unla/rentar/ReservaTest.java
+++ backend/src/test/java/ar/unla/rentar/ReservaTest.java
@@ -2,12 +2,29 @@ package ar.unla.rentar;
 
 
 import ar.unla.rentar.model.EstadoReserva;
+import ar.unla.rentar.model.Reserva;
 import ar.unla.rentar.repository.ReservaRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ReservaTest {
+class ReservaTest {
+
+    @Autowired
+    private ReservaRepository reservaRepository;
+
+    @Test
+    void testCrearReservaSinUsuario() {
+
+        Reserva reserva = new Reserva();
+
+        reserva.setEstado(EstadoReserva.CONFIRMADA);
+
+        
+
+
+
+    }
 
 }
