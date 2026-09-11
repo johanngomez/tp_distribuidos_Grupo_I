c1e0c0c1c5ade72eb7cd787dc346d99ecbd427c0
MicaelaInsfran02 <insfranmica00@gmail.com>
2026-09-10 21:54:40 -0300

    Agrego estructura de navegación con React Router y placeholders de pantallas

---
+++ frontend/src/components/Disponibilidad.jsx
@@ -0,0 +1,5 @@
+function Disponibilidad() {
+    return <h1>Disponibilidad (en construcción)</h1>;
+}
+
+export default Disponibilidad;
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,5 @@
+function Login() {
+    return <h1>Login (esperando rama)</h1>;
+}
+
+export default Login;
