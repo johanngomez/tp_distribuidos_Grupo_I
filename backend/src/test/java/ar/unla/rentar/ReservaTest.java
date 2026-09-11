c582289c2cbd268cb9cd306c6f0c04bd34fefed5
unknown <pauloargibay@gmail.com>
2026-09-11 02:16:06 -0300

    agregar etiqueta transactional a ReservaTest

--- backend/src/test/java/ar/unla/rentar/ReservaTest.java
+++ backend/src/test/java/ar/unla/rentar/ReservaTest.java
@@ -22,7 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-
+@Transactional 
 class ReservaTest {
 
     @Autowired
